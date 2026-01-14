fc5efa9511b934961f15ae92a3665f48ab2ee462
Joe <abishekcpabi@outlook.com>
2026-01-14 19:28:29 +0530

    1. Two Sum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,3 +20,14 @@ public boolean isPalindrome(int x) {
 
     return reverseNum == x;
 }
+public int[] twoSum(int[] nums, int target) {
+    Map <Integer,Integer> complement = new HashMap<>();
+    for (int i = 0; i < nums.length; i++){
+        if (complement.get(nums[i]) != null){
+            return new int[] {complement.get(nums[i]),i};
+        }else{
+            complement.put(target - nums[i], i);
+        }
+    }
+    return nums;
+}
