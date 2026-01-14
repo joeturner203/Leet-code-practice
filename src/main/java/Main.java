425e21c79c5d1bbbf4e0a77b4a72eb51574f7a90
Joe <abishekcpabi@outlook.com>
2026-01-14 19:26:08 +0530

    9. Palindrome Number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,22 @@
 void main() {
-    IO.println("Hello World");
+    IO.println(isPalindrome(2345432));
+}
+
+public boolean isPalindrome(int x) {
+    if(x < 0) return false;
+    if (x == 0) return true;
+    if (x % 10 == 0) return false;
+    int reverseNum = 0;
+
+    while(reverseNum < x) {
+        int digit = x % 10;
+        x = x / 10;
+        reverseNum = reverseNum * 10 + digit;
+    }
+
+    if (reverseNum > x) {
+        reverseNum = reverseNum / 10;
+    }
+
+    return reverseNum == x;
 }
